e38f3a9921fc123d00c05eefb101c7461c2e06e3
Mukul <YourEmail>
2024-10-14 12:52:12 +0200

    initial commit

---
+++ FitnessApp/Migrations/20241014053252_ChangedWorkoutType.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedWorkoutType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Workouts_Users_UserId",
+                table: "Workouts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Workouts_UserId",
+                table: "Workouts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Workouts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "WorkoutId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_WorkoutId",
+                table: "Users",
+                column: "WorkoutId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Workouts_WorkoutId",
+                table: "Users",
+                column: "WorkoutId",
+                principalTable: "Workouts",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Workouts_WorkoutId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_WorkoutId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "WorkoutId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Workouts",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Workouts_UserId",
+                table: "Workouts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Workouts_Users_UserId",
+                table: "Workouts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
